fbfa11276a43dec348338704bfd1e48b2fa22e19
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-12-14 10:16:47 -0600

    Added theme colors to BottomSheet component

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -2,21 +2,45 @@ import React from 'react';
 
 import RBSheet, { RBSheetProps } from 'react-native-raw-bottom-sheet';
 
+import { useTheme } from '../../hooks';
+
 export interface BottomSheetProps extends RBSheetProps {
   ref?: React.Ref<RBSheet>;
   children?: React.ReactNode;
 }
 
 const BottomSheet = React.forwardRef<RBSheet, BottomSheetProps>(
-  ({ children, ...props }, ref?) => {
+  ({ children, customStyles = {}, ...props }, ref?) => {
+    const { palette } = useTheme();
+
     return (
-      <RBSheet {...props} ref={ref}>
+      <RBSheet
+        customStyles={{
+          container: [
+            {
+              backgroundColor: palette.background.container,
+              borderTopLeftRadius: 32,
+              borderTopRightRadius: 32,
+            },
+            customStyles.container,
+          ],
+          draggableIcon: [
+            {
+              backgroundColor: palette.divider,
+              width: 64,
+            },
+            customStyles.draggableIcon,
+          ],
+        }}
+        closeOnDragDown
+        {...props}
+        ref={ref}
+      >
         {children}
       </RBSheet>
     );
   },
 );
-
 export type BottomSheetHandle = React.ElementRef<typeof BottomSheet>;
 
 export default BottomSheet;
