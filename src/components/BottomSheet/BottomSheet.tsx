ecca104940ab910f640269864d832c25d848a51c
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-12-14 11:53:53 -0600

    Created useBottomSheet hook, added visible prop to BottomSheet component

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useImperativeHandle } from 'react';
+import type { StyleProp, ViewStyle } from 'react-native';
 
 import RBSheet, { RBSheetProps } from 'react-native-raw-bottom-sheet';
 
@@ -7,11 +8,30 @@ import { useTheme } from '../../hooks';
 export interface BottomSheetProps extends RBSheetProps {
   ref?: React.Ref<RBSheet>;
   children?: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+  visible?: boolean;
 }
 
-const BottomSheet = React.forwardRef<RBSheet, BottomSheetProps>(
-  ({ children, customStyles = {}, ...props }, ref?) => {
+export type BottomSheetHandle = {
+  open: () => void;
+  close: () => void;
+};
+
+const BottomSheet = React.forwardRef<BottomSheetHandle, BottomSheetProps>(
+  ({ children, customStyles = {}, style, visible, ...props }, ref?) => {
     const { palette } = useTheme();
+
+    const bottomSheetRef = React.useRef<RBSheet>(null);
+
+    useImperativeHandle(ref, () => ({
+      open: () => bottomSheetRef.current?.open(),
+      close: () => bottomSheetRef.current?.close(),
+    }));
+
+    useEffect(() => {
+      if (visible) bottomSheetRef.current?.open();
+      else bottomSheetRef.current?.close();
+    }, [visible]);
 
     return (
       <RBSheet
@@ -21,26 +41,29 @@ const BottomSheet = React.forwardRef<RBSheet, BottomSheetProps>(
               backgroundColor: palette.background.container,
               borderTopLeftRadius: 32,
               borderTopRightRadius: 32,
+              padding: 20,
+              paddingTop: 0,
             },
             customStyles.container,
+            style,
           ],
           draggableIcon: [
             {
               backgroundColor: palette.divider,
               width: 64,
+              marginBottom: 20,
             },
             customStyles.draggableIcon,
           ],
         }}
         closeOnDragDown
         {...props}
-        ref={ref}
+        ref={bottomSheetRef}
       >
         {children}
       </RBSheet>
     );
   },
 );
-export type BottomSheetHandle = React.ElementRef<typeof BottomSheet>;
 
 export default BottomSheet;
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useTheme } from './useTheme';
 export { default as useForm } from './useForm';
 export { default as usePressableStyles } from './usePressableStyles';
+export { default as useBottomSheet } from './useBottomSheet';
