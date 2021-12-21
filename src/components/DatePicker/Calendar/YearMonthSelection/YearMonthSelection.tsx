26a344588b1bfb6dc271acc874c7f7f8351197f6
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-12-21 10:57:07 -0600

    Finished month selection view

--- src/components/DatePicker/Calendar/YearMonthSelection/YearMonthSelection.tsx
+++ src/components/DatePicker/Calendar/YearMonthSelection/YearMonthSelection.tsx
@@ -1,6 +1,9 @@
 import React, { useCallback, useMemo } from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
+import { FlatList } from 'react-native-gesture-handler';
+
+import Button from '../../../Button';
 import { Body } from '../../../../typography';
 
 export interface YearMonthSelectionProps {
@@ -13,7 +16,13 @@ export interface YearMonthSelectionProps {
 
 const YEARS = 50;
 
-function YearMonthSelection({ step, date, locale }: YearMonthSelectionProps) {
+function YearMonthSelection({
+  step,
+  date,
+  locale,
+  onPressMonth,
+  onPressYear,
+}: YearMonthSelectionProps) {
   const { year, month } = useMemo(() => {
     return {
       year: date.getFullYear(),
@@ -30,6 +39,14 @@ function YearMonthSelection({ step, date, locale }: YearMonthSelectionProps) {
     [locale, year],
   );
 
+  const formatDate = useCallback(
+    (date: Date) => {
+      const formattedMonth = formatMonth(date.getMonth());
+      return `${formattedMonth}, ${date.getFullYear()}`;
+    },
+    [formatMonth],
+  );
+
   const years = useMemo(() => {
     const years = [];
     for (let i = year - YEARS; i <= year + YEARS; i++) years.push(i);
@@ -37,14 +54,55 @@ function YearMonthSelection({ step, date, locale }: YearMonthSelectionProps) {
   }, [year]);
 
   const months = useMemo(() => {
-    const months = [];
-    for (let i = 0; i < 12; i++) months.push(formatMonth(i));
+    const months: Array<Array<{ month: number; name: string }>> = [];
+
+    for (let i = 0; i < 4; i++) {
+      const group = [];
+
+      for (let j = 0; j < 3; j++)
+        group.push({
+          month: i * 3 + j,
+          name: formatMonth(i * 3 + j),
+        });
+
+      months.push(group);
+    }
     return months;
   }, [formatMonth]);
 
   if (step === 'year') return <Body>{year}</Body>;
 
-  if (step === 'month') return <Body>{formatMonth(month)}</Body>;
+  if (step === 'month')
+    return (
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Body size="large" color="primary" weight="bold">
+            {year}
+          </Body>
+        </View>
+        <FlatList
+          contentContainerStyle={styles.flatlist}
+          data={months}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({ item }) => {
+            return (
+              <View style={{ flexDirection: 'row' }}>
+                {item.map(({ month: m, name }) => (
+                  <View key={name} style={styles.buttonContainer}>
+                    <Button
+                      onPress={() => onPressMonth?.(m)}
+                      color={m === month ? 'primary' : 'default'}
+                      text={name}
+                    />
+                  </View>
+                ))}
+              </View>
+            );
+          }}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    );
 
   return null;
 }
@@ -52,5 +110,23 @@ function YearMonthSelection({ step, date, locale }: YearMonthSelectionProps) {
 YearMonthSelection.defaultProps = {
   locale: 'en-US',
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  flatlist: {
+    padding: 10,
+  },
+  buttonContainer: {
+    padding: 6,
+    width: '33%',
+  },
+  titleContainer: {
+    padding: 16,
+    paddingBottom: 0,
+    alignItems: 'center',
+  },
+});
 
 export default React.memo(YearMonthSelection);
