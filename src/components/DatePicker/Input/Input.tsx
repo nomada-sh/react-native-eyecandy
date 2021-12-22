31f20f55a744afc8868266f8718fb1d4d56d25fd
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-12-22 08:58:58 -0600

    Version 1.0.5

--- src/components/DatePicker/Input/Input.tsx
+++ src/components/DatePicker/Input/Input.tsx
@@ -1,17 +1,12 @@
+import React from 'react';
 import { View } from 'react-native';
 
 export interface InputProps {
   children?: React.ReactNode;
 }
 
-function Input({
-  children
-}: InputProps) {
-  return (
-    <View>
-      {children}
-    </View>
-  );
+function Input({ children }: InputProps) {
+  return <View>{children}</View>;
 }
 
 export default Input;
