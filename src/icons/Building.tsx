fb94c8e51750aca40e83360beab0c9debd4e7dd7
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-12-29 15:42:48 -0600

    Version 1.0.12

---
+++ src/icons/Building.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Path } from 'react-native-svg';
+
+import Icon, { IconProps } from './Icon';
+
+const Building = (props: IconProps) => (
+  <Icon {...props}>
+    <Path
+      strokeWidth={1}
+      d="M10.75 18.25v3H3.5V2.75h17v18.5h-7.25v-3h-2.5Zm-6.25-15H4v17.5h6.25v-3h3.5v3H20V3.25H4.5Zm4 3h-2v-.5h2v.5Zm4.5 0h-2v-.5h2v.5Zm4.5 0h-2v-.5h2v.5Zm-9 3h-2v-.5h2v.5Zm4.5 0h-2v-.5h2v.5Zm4.5 0h-2v-.5h2v.5Zm-9 3h-2v-.5h2v.5Zm4.5 0h-2v-.5h2v.5Zm4.5 0h-2v-.5h2v.5Zm-9 3h-2v-.5h2v.5Zm4.5 0h-2v-.5h2v.5Zm4.5 0h-2v-.5h2v.5Zm-9 3h-2v-.5h2v.5Zm9 0h-2v-.5h2v.5Z"
+    />
+  </Icon>
+);
+
+export default React.memo(Building);
